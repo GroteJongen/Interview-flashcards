711ad0334a5abb0c83cec22d98ea1df2c167e6f5
GroteJongen <50738377+GroteJongen@users.noreply.github.com>
2020-02-23 21:15:57 +0100

    Update after code review closes #1

--- src/main/java/com/biszczak/marek/Main.java
+++ src/main/java/com/biszczak/marek/Main.java
@@ -3,7 +3,8 @@ package com.biszczak.marek;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        final String helloMessage = "Hello World";
+        System.out.println(helloMessage);
 
     }
 }
