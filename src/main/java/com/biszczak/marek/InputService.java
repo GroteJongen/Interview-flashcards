4e61f73d94b137c48b11b36ddc9eba0b77a67470
GroteJongen <50738377+GroteJongen@users.noreply.github.com>
2020-02-25 19:39:49 +0100

    Add InputService requested methods closes#14

--- src/main/java/com/biszczak/marek/InputService.java
+++ src/main/java/com/biszczak/marek/InputService.java
@@ -2,7 +2,16 @@ package com.biszczak.marek;
 
 public class InputService {
 
-    public InputService() {
+    public String getQuestion() {
+        return null;
+    }
+
+    public String getAnswer() {
+        return null;
+    }
+
+    public String getCommand() {
+        return null;
     }
 
 }
