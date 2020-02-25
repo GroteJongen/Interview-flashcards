fdb2284d4cfff77a8ad5313de6138da808543f87
GroteJongen <50738377+GroteJongen@users.noreply.github.com>
2020-02-25 19:43:47 +0100

    Add inputStrategy interface closes#15

--- src/main/java/com/biszczak/marek/InputService.java
+++ src/main/java/com/biszczak/marek/InputService.java
@@ -2,6 +2,9 @@ package com.biszczak.marek;
 
 public class InputService {
 
+    private InputStrategy inputStrategy;
+
+
     public String getQuestion() {
         return null;
     }
---
+++ src/main/java/com/biszczak/marek/InputStrategy.java
@@ -0,0 +1,5 @@
+package com.biszczak.marek;
+
+public interface InputStrategy {
+
+}
