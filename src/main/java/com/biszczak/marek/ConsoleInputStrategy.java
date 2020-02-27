2a2bf9b348b13e6a7a2df96091391a0570636c5d
GroteJongen <50738377+GroteJongen@users.noreply.github.com>
2020-02-27 20:53:40 +0100

    Change ConsoleInputStrategyMethods closes#20

--- src/main/java/com/biszczak/marek/ConsoleInputStrategy.java
+++ src/main/java/com/biszczak/marek/ConsoleInputStrategy.java
@@ -6,19 +6,16 @@ public class ConsoleInputStrategy implements InputStrategy {
     private Scanner scanner = new Scanner(System.in);
 
     public String getQuestion() {
-        System.out.println("Pass the question");
         String questionFromUsreInput = scanner.nextLine();
         return questionFromUsreInput;
     }
 
     public String getAnswer() {
-        System.out.println("Pass the answer");
         String answerFromUsreInput = scanner.nextLine();
         return answerFromUsreInput;
     }
 
     public String getCommand() {
-        System.out.println("Pass the command");
         String commandFromUsreInput = scanner.nextLine();
         return commandFromUsreInput;
     }
